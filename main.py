8e4fc6bbc5596f802496380b7b0f600a14271583
Frances Calceta <abcalceta@users.noreply.github.com>
2016-12-20 18:47:14 +0800

    added scoring, lives, and gameEnd
    game now ends when lives are zero
    score is displayed at gameEnd
    press any key to exit the game once gameEnd

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import pyglet
 import random
 from Word import Word
 
-difficulty = input("difficulty:")
-wpm = input("wpm:")
-ngrams = input("ngrams:")
-
+# difficulty = input("difficulty:")
+# wpm = input("wpm:")
+# ngrams = input("ngrams:")
+difficulty = 5
+wpm = 50
+ngrams = 2
 
 
 window = pyglet.window.Window(800,600, caption = "Typing of the Dead Inside")
@@ -19,11 +21,14 @@ killPoint = window.height//4
 words = []
 spawnableRange = []
 numRange = 20
+lives = 3
+score = 0
 
 # wpm = 100
 # difficulty = 3
 # ngrams = 4
-
+gameEnd = False
+poppingDone = False
 
 
 
@@ -51,9 +56,19 @@ label2 = pyglet.text.Label(  ("wpm: "+str(wpm))   ,font_name="Courier New",font_
 
 label3 = pyglet.text.Label(  ("only words longer than "+str(ngrams)+ " characters")   ,font_name="Courier New",font_size=20,x=window.width//2, y=killPoint-130,anchor_x='center', anchor_y='bottom', bold=True)
 
+label4 = pyglet.text.Label(  ("lives: "+" "+ " left")   ,font_name="Courier New",font_size=18,x=window.width//2+300, y=killPoint,anchor_x='center', anchor_y='top', bold=False)
+label5 = pyglet.text.Label(  ("score: "+" "+ " points")   ,font_name="Courier New",font_size=18,x=window.width//2+280, y=killPoint-30,anchor_x='center', anchor_y='top', bold=False)
+
+livesLabel = pyglet.text.Label(  ("      "+str(lives)+ "     ")   ,font_name="Courier New",font_size=18,x=window.width//2+300, y=killPoint,anchor_x='center', anchor_y='top', bold=True)
+scoreLabel = pyglet.text.Label(  ("       "+str(score)+ "       ")   ,font_name="Courier New",font_size=18,x=window.width//2+280, y=killPoint-30,anchor_x='center', anchor_y='top', bold=True)
+
+
+gameEndLabel = pyglet.text.Label(  ("Game end, press any key to exit")   ,font_name="Courier New",font_size=28,x=window.width//2, y=window.height//2,anchor_x='center', anchor_y='center', bold=True)
 
 
 fps_display = pyglet.clock.ClockDisplay()
+
+
 # Display here
 @window.event
 def on_draw():
@@ -67,39 +82,81 @@ def on_draw():
 	label1.draw()
 	label2.draw()
 	label3.draw()
+	label4.draw()
+	label5.draw()
+	livesLabel.draw()
+	scoreLabel.draw()
+	global gameEnd
+	global poppingDone
+	if gameEnd:
+		pyglet.gl.glClearColor(0.5, 0.3, 0.3, 0)
+		if poppingDone:
+			gameEndLabel.draw()
+			global score
+			pyglet.text.Label(  ("Final Score: "+str(score))   ,font_name="Courier New",font_size=34,x=window.width//2, y=window.height//2+70,anchor_x='center', anchor_y='center', bold=True).draw()
+	
 
 
 
 @window.event
 def on_text(text):
-	print text
-	for w in words:
-		w.getLetter(text)
+	global gameEnd
+	if not gameEnd:
+		print text
+		for w in words:
+			w.getLetter(text)
+	else:
+		exit()
+		pass
 
 # Update here
 def doChecks(dt):	
-	for x in range(len(words)-1, -1, -1):
-		w = words[x]
-		if w.end:
-			words.pop(x)
-			for word in words:
-				word.currIndex = 0 #don't carry over letters
-			continue
-		if w.yPos < killPoint:
-			#kill it
-			# w.yPos = window.height
-			words.pop(x)
-			continue
+	global gameEnd
+	if not gameEnd:
+		for x in range(len(words)-1, -1, -1):
+			w = words[x]
+			if w.end:
+				words.pop(x)
+				global score
+				global scoreLabel
+				score += 1
+				for word in words:
+					word.currIndex = 0 #don't carry over letters
+				scoreLabel = pyglet.text.Label(  ("       "+str(score)+ "       ")   ,font_name="Courier New",font_size=18,x=window.width//2+280, y=killPoint-30,anchor_x='center', anchor_y='top', bold=True)	
+				continue
+			if w.yPos < killPoint:
+				#kill it
+				# w.yPos = window.height
+				global lives
+				global livesLabel
+				lives -= 1
+				livesLabel = pyglet.text.Label(  ("      "+str(lives)+ "     ")   ,font_name="Courier New",font_size=18,x=window.width//2+300, y=killPoint,anchor_x='center', anchor_y='top', bold=True)
+				if lives<=0:
+					gameEnd = True
+				words.pop(x)
+				global gameEnd
+				continue
 
-		w.checked = True
-		w.updatePos(dt)
+			w.checked = True
+			w.updatePos(dt)
+	else:
+		# gameEndLabel.draw()
+		pass
+
 
 def spawnWord(dt):
-	print "spawning word"
-	velocity = difficulty * -25
-	words.append(Word(  getWord() , random.choice(spawnableRange), window.height,velocity))
-	print "words list len", len(words)
-
+	global gameEnd
+	# print gameEnd
+	if not gameEnd:
+		print "spawning word"
+		velocity = difficulty * -25
+		words.append(Word(  getWord() , random.choice(spawnableRange), window.height,velocity))
+		print "words list len", len(words)
+	else:
+		for x in range(len(words)-1, -1, -1):
+			words.pop(x)
+		global poppingDone
+		poppingDone = True
 
 pyglet.clock.schedule_interval(spawnWord, 60.0/wpm)
 pyglet.clock.schedule_interval(doChecks, 1/60.0)
